d82524edb32862cdb609ec4e5aed6f0ce9d22b76
domingo <dominp97@gmail.com>
2018-03-07 17:13:10 +0100

    Commit inicial

---
+++ ProyectoGitEd/src/paquete1/ClaseMain.java
@@ -0,0 +1,14 @@
+package paquete1;
+
+public class ClaseMain {
+
+	public static void main(String[] args) {
+		int i=0;
+		for(int j = 0;j<5;j++) {
+			i+=j;
+		}
+		System.out.println(i);
+
+	}
+
+}
